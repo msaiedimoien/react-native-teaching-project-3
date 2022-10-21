f238ace3c796c8e318a78de3874267ca5f521d3a
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-10-22 00:01:15 +0330

    Refactor Signup Screen

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Text, Input, Button} from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
-    return(
+    return (
         <>
-            <Text style={{ fontSize: 48 }}>Signup Screen</Text>
-            <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')} />
-            <Button title='Go to main flow' onPress={() => navigation.navigate('mainFlow')} />
+            <Spacer>
+                <Text h3>Sign up for Tracker</Text>
+            </Spacer>
+            <Input label="Email"/>
+            <Spacer/>
+            <Input label="Password"/>
+            <Spacer>
+                <Button title="Sign up"/>
+            </Spacer>
         </>
     )
 };
