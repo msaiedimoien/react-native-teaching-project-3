8d54b2edda546bfdab924eed797722cac6a3eb53
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-10-22 21:36:43 +0330

    create AuthContext and refactor Signup Screen

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'http://f1e5-185-117-88-238.ngrok.io'
+});
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text, Input, Button} from "react-native-elements";
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -30,7 +32,7 @@ const SignupScreen = ({ navigation }) => {
                 autoCorrect={false}
             />
             <Spacer>
-                <Button title="Sign up"/>
+                <Button title="Sign up" onPress={() => signup({email, password})}/>
             </Spacer>
         </View>
     )
