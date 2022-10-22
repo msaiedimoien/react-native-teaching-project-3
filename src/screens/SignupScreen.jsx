f61f132b8a60631c06e52d08c6efe3c5baf689eb
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-10-22 13:18:37 +0330

    Refactor Signup Screen, create AuthProvider and refactor App.js

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Text, Input, Button} from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <>
+        <View style={styles.container}>
             <Spacer>
-                <Text h3>Sign up for Tracker</Text>
+                <Text h4 style={styles.title}>Sign up for Tracker</Text>
             </Spacer>
-            <Input label="Email"/>
             <Spacer/>
-            <Input label="Password"/>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Spacer/>
+            <Input
+                secureTextEntry
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <Spacer>
                 <Button title="Sign up"/>
             </Spacer>
-        </>
+        </View>
     )
 };
 
-const  styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return{
+        header: null
+    };
+};
+
+const  styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 150,
+        padding: 20
+    },
+    title: {
+        textAlign: "center",
+        color: 'gray',
+        fontWeight: "bold"
+    }
+});
 
 export default SignupScreen;
